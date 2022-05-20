456af832e7dd785f205cdc0b25010b5410aaddab
Taheny BELGUITH <taheny.belguith@gmail.com>
2022-05-21 00:27:44 +0100

    Revert "Popup data student b"

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import './style.css';
-import './modules/dependencies.js';
-import { popupDisplay, makeComment } from './modules/popup.js';
-import { getComments } from './modules/updateComments.js';
-import commentsCount from './modules/comments-counter.js';
 import getData from './modules/fetch-api.js';
 
-window.addEventListener('load', async () => {
-  await getData();
-  const popup = document.querySelector('.popup');
-  document.querySelectorAll('.comments').forEach((element) => {
-    element.addEventListener('click', async (e) => {
-      popup.classList.add('active');
-      const { id } = element;
-      const fetchMovies = `https://api.tvmaze.com/shows/${id}`;
-      const fetchShows = await fetch(fetchMovies);
-      const data = await fetchShows.json();
-      popupDisplay(data, id);
-      const commentsArr = await getComments(id);
-      if (commentsCount(commentsArr)) {
-        commentsArr.forEach((comment) => {
-          makeComment(comment);
-        });
-      }
-    });
-  });
+window.addEventListener('load', () => {
+  getData();
 });
--- src/modules/dependencies.js
+++
@@ -1,3 +0,0 @@
-import '@fortawesome/fontawesome-free/js/fontawesome.js';
-import '@fortawesome/fontawesome-free/js/solid.js';
-import '@fortawesome/fontawesome-free/js/regular.js';
