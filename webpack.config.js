65749c98d3998ad09516b8f9d228ed5b05b2fc0e
KanoCode <kanokanombola@gmail.com>
2022-05-21 09:41:05 +0200

    Update webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/Kano-Tah-Movies/',
   },
   devServer: {
     static: './dist',
